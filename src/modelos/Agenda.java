7b68e3f145f1d6813374d3d6a9a6b8bd86b05940
sfontalv19 <serme1009@gmail.com>
2024-10-10 11:03:02 -0500

    agregamos los datos de la agenda

--- src/modelos/Agenda.java
+++ src/modelos/Agenda.java
@@ -2,26 +2,41 @@ package modelos;
 
 import datos.IUtilAgenda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Agenda implements IUtilAgenda {
+
+    int maximo = 10;
+    // creamos arrays
+    Contacto[] listaContactos = new Contacto[maximo];
+    List<Contacto> lista = new ArrayList<>(Arrays.asList(listaContactos));
 
     @Override
     public void agregarContacto(Contacto contacto) {
-        
+
+        if (listaContactos.length < maximo) {
+            lista.add(contacto);
+        }
     }
 
     @Override
     public void existeContacto(Contacto contacto) {
-
+        if (lista.contains(contacto)) {
+            System.out.println("el contacto existe");
+        }
     }
 
     @Override
     public void listarContactos() {
-
+        // mostrar los elementos de la lista
+        lista.stream().forEach(c -> System.out.println(c));
     }
 
     @Override
     public void buscarContacto(String nombre, String apellido) {
-
+        lista.stream().forEach(c -> c.nombre.equalsIgnoreCase(nombre));
     }
 
     @Override
@@ -31,16 +46,24 @@ public class Agenda implements IUtilAgenda {
 
     @Override
     public void modificarTelefono(Contacto contacto) {
-
+        Contacto con = lista.stream().findFirst().get();
     }
 
     @Override
     public void agendaLlena() {
-
+        if (listaContactos.length == maximo) {
+            System.out.println("la lista esta llena");
+        }
     }
 
     @Override
     public void espacioLibres() {
-
+        int contador = 0;
+        for (Contacto c : listaContactos) {
+            if (c.equals(null)) {
+                contador++;
+            }
+        }
+        System.out.println("espacio libre:"+ contador);
     }
 }
